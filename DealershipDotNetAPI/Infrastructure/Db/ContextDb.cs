15d84b18597830bec70ed31fe91c59b281a44edf
Alisson Mayer Medeji <alissonmmedeji@gmail.com>
2024-12-17 15:37:25 -0300

    Add Admisnistrator class, Finish ContextDb, Configure program.cs, appsettings.json, Connected to MySQL, ORM working

--- DealershipDotNetAPI/Infrastructure/Db/ContextDb.cs
+++ DealershipDotNetAPI/Infrastructure/Db/ContextDb.cs
@@ -6,5 +6,27 @@ namespace DealershipDotNetAPI.Infrastructure.Db
     public class ContextDb : DbContext
     {
         public DbSet<Administrator> Administrators { get; set; } = default!;
+
+        public readonly IConfiguration _configurationAppSettings;
+
+        public ContextDb(IConfiguration configurationAppSettings)
+        {
+            _configurationAppSettings = configurationAppSettings;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var connectionString = _configurationAppSettings.GetConnectionString("mysql").ToString();
+
+                if (!string.IsNullOrEmpty(connectionString))
+                {
+                    optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+                }
+            }
+            
+            
+        }
     }
 }
--- DealershipDotNetAPI/Program.cs
+++ DealershipDotNetAPI/Program.cs
@@ -1,3 +1,7 @@
+using DealershipDotNetAPI.Domain.DTOs;
+using DealershipDotNetAPI.Infrastructure.Db;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ContextDb>(options =>
+{
+    options.UseMySql(
+            builder.Configuration.GetConnectionString("mysql"),
+            ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+            );
+});
 
 var app = builder.Build();
 
@@ -23,11 +35,11 @@ app.UseAuthorization();
 app.MapControllers();
 
 //Requisição GET no Endpoint '/'
-app.MapGet("/", () => "Olá Mundo!");
+app.MapGet("/", () => "Hello Word!");
 
 app.MapPost("/login", (LoginDTO loginDTO) =>
 {
-    if (loginDTO.Email == "adm@test.com" && loginDTO.Senha == "1234")
+    if (loginDTO.Email == "adm@test.com" && loginDTO.Password == "1234")
     {
         return Results.Ok("Authorized login");
 
@@ -42,9 +54,4 @@ app.Run();
 //O sufixo DTO é uma
 //convenção que indica que esta classe
 //é um objeto para transferência de dados.
-public class LoginDTO
-{
-    public string Email { get; set; } = default!;
-    public string Senha { get; set; } = default!;
 
-};
