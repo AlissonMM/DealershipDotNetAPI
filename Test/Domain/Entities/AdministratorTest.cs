dfbf9b4ee4899a5981bf58ea8696fe0149b5c40b
Alisson Mayer Medeji <alissonmmedeji@gmail.com>
2025-01-18 18:08:11 -0300

    Add AdmnistratorTest, need to fix the building problem when using test

---
+++ Test/Domain/Entities/AdministratorTest.cs
@@ -0,0 +1,33 @@
+﻿using DealershipDotNetAPI.Domain.Entities;
+using Newtonsoft.Json.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Domain.Entities
+{
+    [TestClass]
+    internal class AdministratorTest
+    {
+        [TestMethod]
+        public void TestGetSetProperties()
+        {
+            //Arrange
+            var adm = new Administrator();
+
+            //Act
+            adm.Id = 10;
+            adm.Email = "test@test.com";
+            adm.Password = "test";
+            adm.Profile = "Adm";
+
+            //Assert
+            Assert.AreEqual(10, adm.Id);
+            Assert.AreEqual("test@test.com", adm.Email);
+            Assert.AreEqual("test", adm.Password);
+            Assert.AreEqual("Adm", adm.Profile);
+        }
+    }
+}
